e1e3e727739031ff8ee92dd108b690ba3498602d
Esjay <shongad@stu.ust.hk>
2015-03-18 23:36:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/team/prototype/Login.java
@@ -0,0 +1,58 @@
+package com.example.team.prototype;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class Login extends ActionBarActivity {
+
+    EditText username, password;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        username=(EditText) findViewById(R.id.editUser);
+        password=(EditText) findViewById(R.id.editPass);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickLogin(View V) {
+        String usernameString=username.getText().toString();
+        String passwordString=password.getText().toString();
+    }
+
+    public void onClickRegister(View V) {
+
+    }
+}
